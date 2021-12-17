d766d11b47de1a31def4af4852efc77202669896
Fernando Valadao <fvaladao@cyral.com>
2021-12-17 11:24:44 -0300

    Update Redshift UDFs

--- masking/redshift/describe/udf.sql
+++ masking/redshift/describe/udf.sql
@@ -1,6 +1,7 @@
 SELECT SPLIT_PART(d.description, '\n', 1) as "Function Name",
        SPLIT_PART(d.description, '\n', 2) as "Version",
-       SPLIT_PART(d.description, '\n', 3) as "Created at"
+       SPLIT_PART(d.description, '\n', 3) as "Created at",
+       SPLIT_PART(d.description, '\n', 4) as "Source Code"
 FROM pg_proc p
     LEFT JOIN pg_description d
     ON d.objoid = p.oid
